9e48456b382dc9311fb3f059c829785b3331fcf3
Scilla Benussi <scillabe@buffalo.edu>
2022-05-09 18:46:01 -0400

    new files

--- cse250Final1.scala
+++ cse250Final1.scala
@@ -4,11 +4,8 @@
 import java.io.{FileWriter, PrintWriter}
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
-case class UserGenreRating(user: String, genre: String, ratingAvg: Double)
 
-class GenreBox extends Cardbox[UserGenreRating]((x,y) => x.user.compareTo(y.user))
-
-object cse250Final extends App{
+object cse250Final1 extends App{
   val movies: ArrayBuffer[MovieEntry] = MovieReader.readMovies
   val userTuple = UserReaders.readEntries
   val userdatabase = userTuple._1
